ee755714b847474c927f74aafb5e3ffbc8276bb7
zhen1asemen1uk <74488342+zhen1asemen1uk@users.noreply.github.com>
2021-02-07 12:56:20 +0200

    t06 done

---
+++ sprint04/t06/js/script.js
@@ -0,0 +1,35 @@
+let enter = document.querySelector(".enter");
+let btn = document.querySelector(".btn");
+let chatBox = document.querySelector(".chatBox");
+let hi = new RegExp("hi", "i"),
+   hello = new RegExp("hello", "i"),
+   privet = new RegExp("привет", "i"),
+   pruvit = new RegExp("привіт", "i"),
+   hey = new RegExp("hey", "i");
+let count = 0;
+
+//клік на відправку
+btn.addEventListener("click", () => {
+   console.log(count);
+   chatBox.innerHTML += `<p class="textIn">${enter.value}</p>`;
+   if (count >= 5 && (hi.test(enter.value) || hello.test(enter.value) || privet.test(enter.value) || pruvit.test(enter.value) || hey.test(enter.value))) {
+      chatBox.innerHTML += `<p class="textOut">I will not respond to that</p>`;
+   } else if (count < 5 && (hi.test(enter.value) || hello.test(enter.value) || privet.test(enter.value) || pruvit.test(enter.value) || hey.test(enter.value))) {
+      count++
+      chatBox.innerHTML += `<p class="textOut">${gen.next().value}</p>`;
+   } else {
+      chatBox.innerHTML += `<p class="textOut">I don't understand
+</p>`;
+   }
+   enter.value = ""
+})
+
+//генератор відповіді
+function* generator() {
+   yield "Hello, I am J.A.R.V.I.S.";
+   yield "I believe I've already said it, but, sure, hello again!";
+   yield "You are malfunctioning";
+   yield "I believe your intentions to be hostile.";
+   yield "I will not respond to that";
+}
+const gen = generator();
---
+++ sprint04/t07/js/script.js
@@ -0,0 +1,25 @@
+let user = {
+   name: document.getElementById('name').value,
+   age: document.getElementById('age').value,
+   email: document.getElementById('email').value
+};
+
+// Don't edit above this line
+
+// Don't edit below this line
+
+function edit(btn) {
+   btn.innerHTML = 'save';
+   btn.setAttribute('onclick', 'save(this)');
+   const input = document.getElementById(btn.previousElementSibling.id);
+   input.removeAttribute('disabled');
+}
+
+function save(btn) {
+   btn.innerHTML = 'edit';
+   btn.setAttribute('onclick', 'edit(this)');
+   const input = document.getElementById(btn.previousElementSibling.id);
+   input.setAttribute('disabled', 'true');
+   user[input.id] = document.getElementById(input.id).value;
+   document.getElementById(input.id).value = user[input.id];
+}
